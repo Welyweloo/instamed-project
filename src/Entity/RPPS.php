e91bc727e12b96df0c559b83fbf7c30dd8d56fcd
Aurelie ANGLIO <aurelieanglio@MacBook-Pro-de-Aurelie.local>
2020-11-25 00:36:49 +0100

    Created index on id_rpps to help load file faster

--- src/Entity/RPPS.php
+++ src/Entity/RPPS.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass=RPPSRepository::class)
+ * @ORM\Table(name="rpps",indexes={@ORM\Index(name="rpps_index", columns={"id_rpps"})})
  */
 class RPPS
 {
